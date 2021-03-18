7b306d3698ba8040e06c49643c43dfaebaa0e2c6
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-03-18 18:31:25 +0600

    New TestNG Works

---
+++ TestNGTutorial/src/test/Day1.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}*/
+	
+	@Test
+	public void Demo(){
+		
+		System.out.println("hello");
+		
+	}
+
+	@Test
+	public void Demo2(){
+		
+		System.out.println("Bye");
+		
+	}
+
+}
---
+++ TestNGTutorial/src/test/Day2.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+@Test
+
+public class Day2 {
+	
+	public void demo() {
+		System.out.println("Hello from day 2");
+	}
+
+}
