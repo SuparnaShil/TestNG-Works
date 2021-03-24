0414604da2ea3a84748a4d3e44f28d2606e65f45
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-03-24 12:45:42 +0600

    Data provider

--- TestNGTutorial/src/test/Day3.java
+++ TestNGTutorial/src/test/Day3.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -41,4 +42,58 @@ public void loanFinalww(String urlName,String password)
 		System.out.println(urlName);
 		System.out.println(password);
 	}
+
+@Test(dataProvider="getData")
+public void login(String us, String ps)    //data provider class send data here
+	{
+	System.out.println(us);
+	System.out.println(ps);
+	}
+
+@DataProvider
+public Object[][] getData()
+	{
+		//dataset 1st combination  - username password -good credit history
+		//dataset 2nd combination - username password -no credit history
+		//dataset 3rd combination - fraudline history
+	
+	
+		Object[][] data = new Object[3][2];   //3 combination 2 values for each combination
+		
+		//1st set
+		data[0][0]="Fristname";
+		data[0][1]="pass1";
+		
+		//2nd set
+		data[1][0]="Secondname";
+		data[1][1]="pass2";
+		
+		//3rd set
+		data[2][0]="Thirdname";
+		data[2][1]="pass3";
+		
+		return data;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
